80665385617fb35bbb4508aa540dc3f15e91ba78
EmotionIsMe <616356667@qq.com>
2023-02-23 22:07:56 +0800

    Add OpenGL context class

--- TinyEngine/src/Platform/Windows/WindowsWindow.h
+++ TinyEngine/src/Platform/Windows/WindowsWindow.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "tepch.h"
 #include "TinyEngine/Window.h"
+#include "TinyEngine/Renderer/GraphicsContext.h"
 
 #include "GLFW/glfw3.h"
 
@@ -28,6 +29,8 @@ namespace TinyEngine {
 		virtual void Shutdown();
 	private:
 		GLFWwindow* m_Window;
+		GraphicsContext* m_Context;
+
 		struct WindowData
 		{
 			std::string Title;
