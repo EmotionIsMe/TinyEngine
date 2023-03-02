41113f8692c3f0211157ec7778c3fd94b9cfc1a7
EmotionIsMe <616356667@qq.com>
2023-03-02 22:46:09 +0800

    Moved OpenGL shader implementation to OpenGLShader class

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,8 +1,12 @@
 #include <TinyEngine.h>
 
+#include "Platform/OpenGL/OpenGLShader.h"
+
 #include "imgui/imgui.h"
 
 #include <glm/gtc/matrix_transform.hpp>
+
+#include <glm/gtc/type_ptr.hpp>
 
 class ExampleLayer : public TinyEngine::Layer {
 
@@ -83,9 +87,9 @@ public:
 			}
 		)";
 
-		m_Shader.reset(new TinyEngine::Shader(vertexSrc, fragmentSrc));
+		m_Shader.reset(TinyEngine::Shader::Create(vertexSrc, fragmentSrc));
 
-		std::string blueShaderVertexSrc = R"(
+		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
@@ -99,18 +103,20 @@ public:
 			}
 		)";
 
-		std::string blueShaderFragmentSrc = R"(
+		std::string flatColorShaderFragmentSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) out vec4 color;
 			in vec3 v_Position;
+			uniform vec3 u_Color;
+
 			void main()
 			{
-				color = vec4(0.2, 0.3, 0.8, 1.0);
+				color = vec4(u_Color, 1.0);
 			}
 		)";
 
-		m_BlueShader.reset(new TinyEngine::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
+		m_FlatColorShader.reset(TinyEngine::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 	}
 	
 	void OnUpdate(TinyEngine::Timestep ts) override
@@ -140,13 +146,16 @@ public:
 
 		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 
+		std::dynamic_pointer_cast<TinyEngine::OpenGLShader>(m_FlatColorShader)->Bind();
+		std::dynamic_pointer_cast<TinyEngine::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat3("u_Color", m_SquareColor);
+
 		for (int y = 0; y < 20; y++)
 		{
 			for (int x = 0; x < 20; x++)
 			{
 				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
 				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
-				TinyEngine::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+				TinyEngine::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
 			}
 		}
 		TinyEngine::Renderer::Submit(m_Shader, m_VertexArray);
@@ -156,7 +165,9 @@ public:
 
 	virtual void OnImGuiRender() override
 	{
-
+		ImGui::Begin("Settings");
+		ImGui::ColorEdit3("Square Color", glm::value_ptr(m_SquareColor));
+		ImGui::End();
 	}
 
 	void OnEvent(TinyEngine::Event& event) override 
@@ -167,7 +178,7 @@ public:
 		std::shared_ptr<TinyEngine::Shader> m_Shader;
 		std::shared_ptr<TinyEngine::VertexArray> m_VertexArray;
 
-		std::shared_ptr<TinyEngine::Shader> m_BlueShader;
+		std::shared_ptr<TinyEngine::Shader> m_FlatColorShader;
 		std::shared_ptr<TinyEngine::VertexArray> m_SquareVA;
 
 		TinyEngine::OrthographicCamera m_Camera;
@@ -176,6 +187,8 @@ public:
 
 		float m_CameraRotation = 0.0f;
 		float m_CameraRotationSpeed = 180.0f;
+
+		glm::vec3 m_SquareColor = { 0.2f, 0.3f, 0.8f };
 };
 class Sandbox : public TinyEngine::Application
 {
--- TinyEngine/src/TinyEngine/Renderer/Renderer.h
+++ TinyEngine/src/TinyEngine/Renderer/Renderer.h
@@ -23,7 +23,7 @@ namespace TinyEngine {
 			glm::mat4 ViewProjectionMatrix;
 		};
 
-		static SceneData* m_SceneData;
+		static SceneData* s_SceneData;
 	};
 
 }
