6128506351ed72a2091da8eee68dd3575abf1103
EmotionIsMe <616356667@qq.com>
2023-03-07 22:11:30 +0800

    Shaders can now be read from files

--- TinyEngine/src/TinyEngine/Renderer/Shader.cpp
+++ TinyEngine/src/TinyEngine/Renderer/Shader.cpp
@@ -10,6 +10,18 @@
 
 namespace TinyEngine {
 
+	Shader* Shader::Create(const std::string& filepath)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:    TE_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+		case RendererAPI::API::OpenGL:  return new OpenGLShader(filepath);
+		}
+
+		TE_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
 	Shader* Shader::Create(const std::string& vertexSrc, const std::string& fragmentSrc)
 	{
 		switch (Renderer::GetAPI())
