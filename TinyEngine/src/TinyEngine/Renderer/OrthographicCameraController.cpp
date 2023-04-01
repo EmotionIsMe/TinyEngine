64cabd33085cea828d2b29f25d1dfe2752e70fcb
EmotionIsMe <616356667@qq.com>
2023-04-01 18:29:04 +0800

    created viewport panel for scene

--- TinyEngine/src/TinyEngine/Renderer/OrthographicCameraController.cpp
+++ TinyEngine/src/TinyEngine/Renderer/OrthographicCameraController.cpp
@@ -58,6 +58,11 @@ namespace TinyEngine {
 		dispatcher.Dispatch<WindowResizeEvent>(TE_BIND_EVENT_FN(OrthographicCameraController::OnWindowResized));
 	}
 
+	void OrthographicCameraController::OnResize(float width, float height)
+	{
+		m_AspectRatio = width / height;
+		m_Camera.SetProjection(-m_AspectRatio * m_ZoomLevel, m_AspectRatio * m_ZoomLevel, -m_ZoomLevel, m_ZoomLevel);
+	}
 
 	bool OrthographicCameraController::OnMouseScrolled(MouseScrolledEvent& e)
 	{
@@ -74,8 +79,7 @@ namespace TinyEngine {
 	{
 		TE_PROFILE_FUNCTION();
 
-		m_AspectRatio = (float)e.GetWidth() / (float)e.GetHeight();
-		m_Camera.SetProjection(-m_AspectRatio * m_ZoomLevel, m_AspectRatio * m_ZoomLevel, -m_ZoomLevel, m_ZoomLevel);
+		OnResize((float)e.GetWidth(), (float)e.GetHeight());
 		return false;
 	}
 
