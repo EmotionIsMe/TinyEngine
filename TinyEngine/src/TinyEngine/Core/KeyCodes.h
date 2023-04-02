49f6776ca4e5304ed09d8697390867702bcb8541
EmotionIsMe <616356667@qq.com>
2023-04-02 22:39:42 +0800

    Refactored Input situation, change KeyCodes and mouseButtonCodes

--- TinyEngine/src/TinyEngine/Core/KeyCodes.h
+++ TinyEngine/src/TinyEngine/Core/KeyCodes.h
@@ -1,125 +1,273 @@
 #pragma once
 
+namespace TinyEngine
+{
+	typedef enum class KeyCode : uint16_t
+	{
+		// From glfw3.h
+		Space = 32,
+		Apostrophe = 39, /* ' */
+		Comma = 44, /* , */
+		Minus = 45, /* - */
+		Period = 46, /* . */
+		Slash = 47, /* / */
+
+		D0 = 48, /* 0 */
+		D1 = 49, /* 1 */
+		D2 = 50, /* 2 */
+		D3 = 51, /* 3 */
+		D4 = 52, /* 4 */
+		D5 = 53, /* 5 */
+		D6 = 54, /* 6 */
+		D7 = 55, /* 7 */
+		D8 = 56, /* 8 */
+		D9 = 57, /* 9 */
+
+		Semicolon = 59, /* ; */
+		Equal = 61, /* = */
+
+		A = 65,
+		B = 66,
+		C = 67,
+		D = 68,
+		E = 69,
+		F = 70,
+		G = 71,
+		H = 72,
+		I = 73,
+		J = 74,
+		K = 75,
+		L = 76,
+		M = 77,
+		N = 78,
+		O = 79,
+		P = 80,
+		Q = 81,
+		R = 82,
+		S = 83,
+		T = 84,
+		U = 85,
+		V = 86,
+		W = 87,
+		X = 88,
+		Y = 89,
+		Z = 90,
+
+		LeftBracket = 91,  /* [ */
+		Backslash = 92,  /* \ */
+		RightBracket = 93,  /* ] */
+		GraveAccent = 96,  /* ` */
+
+		World1 = 161, /* non-US #1 */
+		World2 = 162, /* non-US #2 */
+
+		/* Function keys */
+		Escape = 256,
+		Enter = 257,
+		Tab = 258,
+		Backspace = 259,
+		Insert = 260,
+		Delete = 261,
+		Right = 262,
+		Left = 263,
+		Down = 264,
+		Up = 265,
+		PageUp = 266,
+		PageDown = 267,
+		Home = 268,
+		End = 269,
+		CapsLock = 280,
+		ScrollLock = 281,
+		NumLock = 282,
+		PrintScreen = 283,
+		Pause = 284,
+		F1 = 290,
+		F2 = 291,
+		F3 = 292,
+		F4 = 293,
+		F5 = 294,
+		F6 = 295,
+		F7 = 296,
+		F8 = 297,
+		F9 = 298,
+		F10 = 299,
+		F11 = 300,
+		F12 = 301,
+		F13 = 302,
+		F14 = 303,
+		F15 = 304,
+		F16 = 305,
+		F17 = 306,
+		F18 = 307,
+		F19 = 308,
+		F20 = 309,
+		F21 = 310,
+		F22 = 311,
+		F23 = 312,
+		F24 = 313,
+		F25 = 314,
+
+		/* Keypad */
+		KP0 = 320,
+		KP1 = 321,
+		KP2 = 322,
+		KP3 = 323,
+		KP4 = 324,
+		KP5 = 325,
+		KP6 = 326,
+		KP7 = 327,
+		KP8 = 328,
+		KP9 = 329,
+		KPDecimal = 330,
+		KPDivide = 331,
+		KPMultiply = 332,
+		KPSubtract = 333,
+		KPAdd = 334,
+		KPEnter = 335,
+		KPEqual = 336,
+
+		LeftShift = 340,
+		LeftControl = 341,
+		LeftAlt = 342,
+		LeftSuper = 343,
+		RightShift = 344,
+		RightControl = 345,
+		RightAlt = 346,
+		RightSuper = 347,
+		Menu = 348
+	} Key;
+
+	inline std::ostream& operator<<(std::ostream& os, KeyCode keyCode)
+	{
+		os << static_cast<int32_t>(keyCode);
+		return os;
+	}
+}
+
 /* Printable keys */
-#define TE_KEY_SPACE              32
-#define TE_KEY_APOSTROPHE         39  /* ' */
-#define TE_KEY_COMMA              44  /* , */
-#define TE_KEY_MINUS              45  /* - */
-#define TE_KEY_PERIOD             46  /* . */
-#define TE_KEY_SLASH              47  /* / */
-#define TE_KEY_0                  48
-#define TE_KEY_1                  49
-#define TE_KEY_2                  50
-#define TE_KEY_3                  51
-#define TE_KEY_4                  52
-#define TE_KEY_5                  53
-#define TE_KEY_6                  54
-#define TE_KEY_7                  55
-#define TE_KEY_8                  56
-#define TE_KEY_9                  57
-#define TE_KEY_SEMICOLON          59  /* ; */
-#define TE_KEY_EQUAL              61  /* = */
-#define TE_KEY_A                  65
-#define TE_KEY_B                  66
-#define TE_KEY_C                  67
-#define TE_KEY_D                  68
-#define TE_KEY_E                  69
-#define TE_KEY_F                  70
-#define TE_KEY_G                  71
-#define TE_KEY_H                  72
-#define TE_KEY_I                  73
-#define TE_KEY_J                  74
-#define TE_KEY_K                  75
-#define TE_KEY_L                  76
-#define TE_KEY_M                  77
-#define TE_KEY_N                  78
-#define TE_KEY_O                  79
-#define TE_KEY_P                  80
-#define TE_KEY_Q                  81
-#define TE_KEY_R                  82
-#define TE_KEY_S                  83
-#define TE_KEY_T                  84
-#define TE_KEY_U                  85
-#define TE_KEY_V                  86
-#define TE_KEY_W                  87
-#define TE_KEY_X                  88
-#define TE_KEY_Y                  89
-#define TE_KEY_Z                  90
-#define TE_KEY_LEFT_BRACKET       91  /* [ */
-#define TE_KEY_BACKSLASH          92  /* \ */
-#define TE_KEY_RIGHT_BRACKET      93  /* ] */
-#define TE_KEY_GRAVE_ACCENT       96  /* ` */
-#define TE_KEY_WORLD_1            161 /* non-US #1 */
-#define TE_KEY_WORLD_2            162 /* non-US #2 */
+// From glfw3.h
+#define TE_KEY_SPACE           ::TinyEngine::Key::Space
+#define TE_KEY_APOSTROPHE      ::TinyEngine::Key::Apostrophe    /* ' */
+#define TE_KEY_COMMA           ::TinyEngine::Key::Comma         /* , */
+#define TE_KEY_MINUS           ::TinyEngine::Key::Minus         /* - */
+#define TE_KEY_PERIOD          ::TinyEngine::Key::Period        /* . */
+#define TE_KEY_SLASH           ::TinyEngine::Key::Slash         /* / */
+#define TE_KEY_0               ::TinyEngine::Key::D0
+#define TE_KEY_1               ::TinyEngine::Key::D1
+#define TE_KEY_2               ::TinyEngine::Key::D2
+#define TE_KEY_3               ::TinyEngine::Key::D3
+#define TE_KEY_4               ::TinyEngine::Key::D4
+#define TE_KEY_5               ::TinyEngine::Key::D5
+#define TE_KEY_6               ::TinyEngine::Key::D6
+#define TE_KEY_7               ::TinyEngine::Key::D7
+#define TE_KEY_8               ::TinyEngine::Key::D8
+#define TE_KEY_9               ::TinyEngine::Key::D9
+#define TE_KEY_SEMICOLON       ::TinyEngine::Key::Semicolon     /* ; */
+#define TE_KEY_EQUAL           ::TinyEngine::Key::Equal         /* = */
+#define TE_KEY_A               ::TinyEngine::Key::A
+#define TE_KEY_B               ::TinyEngine::Key::B
+#define TE_KEY_C               ::TinyEngine::Key::C
+#define TE_KEY_D               ::TinyEngine::Key::D
+#define TE_KEY_E               ::TinyEngine::Key::E
+#define TE_KEY_F               ::TinyEngine::Key::F
+#define TE_KEY_G               ::TinyEngine::Key::G
+#define TE_KEY_H               ::TinyEngine::Key::H
+#define TE_KEY_I               ::TinyEngine::Key::I
+#define TE_KEY_J               ::TinyEngine::Key::J
+#define TE_KEY_K               ::TinyEngine::Key::K
+#define TE_KEY_L               ::TinyEngine::Key::L
+#define TE_KEY_M               ::TinyEngine::Key::M
+#define TE_KEY_N               ::TinyEngine::Key::N
+#define TE_KEY_O               ::TinyEngine::Key::O
+#define TE_KEY_P               ::TinyEngine::Key::P
+#define TE_KEY_Q               ::TinyEngine::Key::Q
+#define TE_KEY_R               ::TinyEngine::Key::R
+#define TE_KEY_S               ::TinyEngine::Key::S
+#define TE_KEY_T               ::TinyEngine::Key::T
+#define TE_KEY_U               ::TinyEngine::Key::U
+#define TE_KEY_V               ::TinyEngine::Key::V
+#define TE_KEY_W               ::TinyEngine::Key::W
+#define TE_KEY_X               ::TinyEngine::Key::X
+#define TE_KEY_Y               ::TinyEngine::Key::Y
+#define TE_KEY_Z               ::TinyEngine::Key::Z
+#define TE_KEY_LEFT_BRACKET    ::TinyEngine::Key::LeftBracket   /* [ */
+#define TE_KEY_BACKSLASH       ::TinyEngine::Key::Backslash     /* \ */
+#define TE_KEY_RIGHT_BRACKET   ::TinyEngine::Key::RightBracket  /* ] */
+#define TE_KEY_GRAVE_ACCENT    ::TinyEngine::Key::GraveAccent   /* ` */
+#define TE_KEY_WORLD_1         ::TinyEngine::Key::World1        /* non-US #1 */
+#define TE_KEY_WORLD_2         ::TinyEngine::Key::World2        /* non-US #2 */
 
 /* Function keys */
-#define TE_KEY_ESCAPE             256
-#define TE_KEY_ENTER              257
-#define TE_KEY_TAB                258
-#define TE_KEY_BACKSPACE          259
-#define TE_KEY_INSERT             260
-#define TE_KEY_DELETE             261
-#define TE_KEY_RIGHT              262
-#define TE_KEY_LEFT               263
-#define TE_KEY_DOWN               264
-#define TE_KEY_UP                 265
-#define TE_KEY_PAGE_UP            266
-#define TE_KEY_PAGE_DOWN          267
-#define TE_KEY_HOME               268
-#define TE_KEY_END                269
-#define TE_KEY_CAPS_LOCK          280
-#define TE_KEY_SCROLL_LOCK        281
-#define TE_KEY_NUM_LOCK           282
-#define TE_KEY_PRINT_SCREEN       283
-#define TE_KEY_PAUSE              284
-#define TE_KEY_F1                 290
-#define TE_KEY_F2                 291
-#define TE_KEY_F3                 292
-#define TE_KEY_F4                 293
-#define TE_KEY_F5                 294
-#define TE_KEY_F6                 295
-#define TE_KEY_F7                 296
-#define TE_KEY_F8                 297
-#define TE_KEY_F9                 298
-#define TE_KEY_F10                299
-#define TE_KEY_F11                300
-#define TE_KEY_F12                301
-#define TE_KEY_F13                302
-#define TE_KEY_F14                303
-#define TE_KEY_F15                304
-#define TE_KEY_F16                305
-#define TE_KEY_F17                306
-#define TE_KEY_F18                307
-#define TE_KEY_F19                308
-#define TE_KEY_F20                309
-#define TE_KEY_F21                310
-#define TE_KEY_F22                311
-#define TE_KEY_F23                312
-#define TE_KEY_F24                313
-#define TE_KEY_F25                314
-#define TE_KEY_KP_0               320
-#define TE_KEY_KP_1               321
-#define TE_KEY_KP_2               322
-#define TE_KEY_KP_3               323
-#define TE_KEY_KP_4               324
-#define TE_KEY_KP_5               325
-#define TE_KEY_KP_6               326
-#define TE_KEY_KP_7               327
-#define TE_KEY_KP_8               328
-#define TE_KEY_KP_9               329
-#define TE_KEY_KP_DECIMAL         330
-#define TE_KEY_KP_DIVIDE          331
-#define TE_KEY_KP_MULTIPLY        332
-#define TE_KEY_KP_SUBTRACT        333
-#define TE_KEY_KP_ADD             334
-#define TE_KEY_KP_ENTER           335
-#define TE_KEY_KP_EQUAL           336
-#define TE_KEY_LEFT_SHIFT         340
-#define TE_KEY_LEFT_CONTROL       341
-#define TE_KEY_LEFT_ALT           342
-#define TE_KEY_LEFT_SUPER         343
-#define TE_KEY_RIGHT_SHIFT        344
-#define TE_KEY_RIGHT_CONTROL      345
-#define TE_KEY_RIGHT_ALT          346
-#define TE_KEY_RIGHT_SUPER        347
-#define TE_KEY_MENU               348
+#define TE_KEY_ESCAPE          ::TinyEngine::Key::Escape
+#define TE_KEY_ENTER           ::TinyEngine::Key::Enter
+#define TE_KEY_TAB             ::TinyEngine::Key::Tab
+#define TE_KEY_BACKSPACE       ::TinyEngine::Key::Backspace
+#define TE_KEY_INSERT          ::TinyEngine::Key::Insert
+#define TE_KEY_DELETE          ::TinyEngine::Key::Delete
+#define TE_KEY_RIGHT           ::TinyEngine::Key::Right
+#define TE_KEY_LEFT            ::TinyEngine::Key::Left
+#define TE_KEY_DOWN            ::TinyEngine::Key::Down
+#define TE_KEY_UP              ::TinyEngine::Key::Up
+#define TE_KEY_PAGE_UP         ::TinyEngine::Key::PageUp
+#define TE_KEY_PAGE_DOWN       ::TinyEngine::Key::PageDown
+#define TE_KEY_HOME            ::TinyEngine::Key::Home
+#define TE_KEY_END             ::TinyEngine::Key::End
+#define TE_KEY_CAPS_LOCK       ::TinyEngine::Key::CapsLock
+#define TE_KEY_SCROLL_LOCK     ::TinyEngine::Key::ScrollLock
+#define TE_KEY_NUM_LOCK        ::TinyEngine::Key::NumLock
+#define TE_KEY_PRINT_SCREEN    ::TinyEngine::Key::PrintScreen
+#define TE_KEY_PAUSE           ::TinyEngine::Key::Pause
+#define TE_KEY_F1              ::TinyEngine::Key::F1
+#define TE_KEY_F2              ::TinyEngine::Key::F2
+#define TE_KEY_F3              ::TinyEngine::Key::F3
+#define TE_KEY_F4              ::TinyEngine::Key::F4
+#define TE_KEY_F5              ::TinyEngine::Key::F5
+#define TE_KEY_F6              ::TinyEngine::Key::F6
+#define TE_KEY_F7              ::TinyEngine::Key::F7
+#define TE_KEY_F8              ::TinyEngine::Key::F8
+#define TE_KEY_F9              ::TinyEngine::Key::F9
+#define TE_KEY_F10             ::TinyEngine::Key::F10
+#define TE_KEY_F11             ::TinyEngine::Key::F11
+#define TE_KEY_F12             ::TinyEngine::Key::F12
+#define TE_KEY_F13             ::TinyEngine::Key::F13
+#define TE_KEY_F14             ::TinyEngine::Key::F14
+#define TE_KEY_F15             ::TinyEngine::Key::F15
+#define TE_KEY_F16             ::TinyEngine::Key::F16
+#define TE_KEY_F17             ::TinyEngine::Key::F17
+#define TE_KEY_F18             ::TinyEngine::Key::F18
+#define TE_KEY_F19             ::TinyEngine::Key::F19
+#define TE_KEY_F20             ::TinyEngine::Key::F20
+#define TE_KEY_F21             ::TinyEngine::Key::F21
+#define TE_KEY_F22             ::TinyEngine::Key::F22
+#define TE_KEY_F23             ::TinyEngine::Key::F23
+#define TE_KEY_F24             ::TinyEngine::Key::F24
+#define TE_KEY_F25             ::TinyEngine::Key::F25
+
+/* Keypad */
+#define TE_KEY_KP_0            ::TinyEngine::Key::KP0
+#define TE_KEY_KP_1            ::TinyEngine::Key::KP1
+#define TE_KEY_KP_2            ::TinyEngine::Key::KP2
+#define TE_KEY_KP_3            ::TinyEngine::Key::KP3
+#define TE_KEY_KP_4            ::TinyEngine::Key::KP4
+#define TE_KEY_KP_5            ::TinyEngine::Key::KP5
+#define TE_KEY_KP_6            ::TinyEngine::Key::KP6
+#define TE_KEY_KP_7            ::TinyEngine::Key::KP7
+#define TE_KEY_KP_8            ::TinyEngine::Key::KP8
+#define TE_KEY_KP_9            ::TinyEngine::Key::KP9
+#define TE_KEY_KP_DECIMAL      ::TinyEngine::Key::KPDecimal
+#define TE_KEY_KP_DIVIDE       ::TinyEngine::Key::KPDivide
+#define TE_KEY_KP_MULTIPLY     ::TinyEngine::Key::KPMultiply
+#define TE_KEY_KP_SUBTRACT     ::TinyEngine::Key::KPSubtract
+#define TE_KEY_KP_ADD          ::TinyEngine::Key::KPAdd
+#define TE_KEY_KP_ENTER        ::TinyEngine::Key::KPEnter
+#define TE_KEY_KP_EQUAL        ::TinyEngine::Key::KPEqual
+
+#define TE_KEY_LEFT_SHIFT      ::TinyEngine::Key::LeftShift
+#define TE_KEY_LEFT_CONTROL    ::TinyEngine::Key::LeftControl
+#define TE_KEY_LEFT_ALT        ::TinyEngine::Key::LeftAlt
+#define TE_KEY_LEFT_SUPER      ::TinyEngine::Key::LeftSuper
+#define TE_KEY_RIGHT_SHIFT     ::TinyEngine::Key::RightShift
+#define TE_KEY_RIGHT_CONTROL   ::TinyEngine::Key::RightControl
+#define TE_KEY_RIGHT_ALT       ::TinyEngine::Key::RightAlt
+#define TE_KEY_RIGHT_SUPER     ::TinyEngine::Key::RightSuper
+#define TE_KEY_MENU            ::TinyEngine::Key::Menu
--- TinyEngine/src/TinyEngine/Core/MouseButtonCodes.h
+++ TinyEngine/src/TinyEngine/Core/MouseButtonCodes.h
@@ -1,15 +1,44 @@
 #pragma once
 
 
-#define TE_MOUSE_BUTTON_1         0
-#define TE_MOUSE_BUTTON_2         1
-#define TE_MOUSE_BUTTON_3         2
-#define TE_MOUSE_BUTTON_4         3
-#define TE_MOUSE_BUTTON_5         4
-#define TE_MOUSE_BUTTON_6         5
-#define TE_MOUSE_BUTTON_7         6
-#define TE_MOUSE_BUTTON_8         7
-#define TE_MOUSE_BUTTON_LAST      TE_MOUSE_BUTTON_8
-#define TE_MOUSE_BUTTON_LEFT      TE_MOUSE_BUTTON_1
-#define TE_MOUSE_BUTTON_RIGHT     TE_MOUSE_BUTTON_2
-#define TE_MOUSE_BUTTON_MIDDLE    TE_MOUSE_BUTTON_3
+#pragma once
+
+namespace TinyEngine
+{
+	typedef enum class MouseCode : uint16_t
+	{
+		// From glfw3.h
+		Button0 = 0,
+		Button1 = 1,
+		Button2 = 2,
+		Button3 = 3,
+		Button4 = 4,
+		Button5 = 5,
+		Button6 = 6,
+		Button7 = 7,
+
+		ButtonLast = Button7,
+		ButtonLeft = Button0,
+		ButtonRight = Button1,
+		ButtonMiddle = Button2
+	} Mouse;
+
+	inline std::ostream& operator<<(std::ostream& os, MouseCode mouseCode)
+	{
+		os << static_cast<int32_t>(mouseCode);
+		return os;
+	}
+}
+
+#define TE_MOUSE_BUTTON_0      ::TinyEngine::Mouse::Button0
+#define TE_MOUSE_BUTTON_1      ::TinyEngine::Mouse::Button1
+#define TE_MOUSE_BUTTON_2      ::TinyEngine::Mouse::Button2
+#define TE_MOUSE_BUTTON_3      ::TinyEngine::Mouse::Button3
+#define TE_MOUSE_BUTTON_4      ::TinyEngine::Mouse::Button4
+#define TE_MOUSE_BUTTON_5      ::TinyEngine::Mouse::Button5
+#define TE_MOUSE_BUTTON_6      ::TinyEngine::Mouse::Button6
+#define TE_MOUSE_BUTTON_7      ::TinyEngine::Mouse::Button7
+#define TE_MOUSE_BUTTON_LAST   ::TinyEngine::Mouse::ButtonLast
+#define TE_MOUSE_BUTTON_LEFT   ::TinyEngine::Mouse::ButtonLeft
+#define TE_MOUSE_BUTTON_RIGHT  ::TinyEngine::Mouse::ButtonRight
+#define TE_MOUSE_BUTTON_MIDDLE ::TinyEngine::Mouse::ButtonMiddle
