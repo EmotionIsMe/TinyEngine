218facd5ae4437bdb9cd567afee29ce750267db3
EmotionIsMe <616356667@qq.com>
2023-04-12 00:10:11 +0800

    Changed ScriptableEntity functions to be virtual

--- Tiny-Editor/src/EditorLayer.cpp
+++ Tiny-Editor/src/EditorLayer.cpp
@@ -43,6 +43,8 @@ namespace TinyEngine {
 		public:
 			void OnCreate()
 			{
+				auto& transform = GetComponent<TransformComponent>().Transform;
+				transform[3][0] = rand() % 10 - 5.0f;
 			}
 
 			void OnDestroy()
@@ -66,7 +68,7 @@ namespace TinyEngine {
 		};
 
 		m_CameraEntity.AddComponent<NativeScriptComponent>().Bind<CameraController>();
-
+		m_SecondCamera.AddComponent<NativeScriptComponent>().Bind<CameraController>();
 	}
 
 	void EditorLayer::OnDetach()
