3a75b4596901e705b91d8be59907514ec0433036
EmotionIsMe <616356667@qq.com>
2023-04-16 22:57:51 +0800

    Added UI to add/remove entities and components + fixed camera bug when added during runtime

--- TinyEngine/src/TinyEngine/Scene/Scene.h
+++ TinyEngine/src/TinyEngine/Scene/Scene.h
@@ -15,12 +15,15 @@ namespace TinyEngine {
 		~Scene();
 
 		Entity CreateEntity(const std::string& name = std::string());
-
-		// TEMP
-		entt::registry& Reg() { return m_Registry; }
+		void DestroyEntity(Entity entity);
 
 		void OnUpdate(Timestep ts);
 		void OnViewportResize(uint32_t width, uint32_t height);
+
+	private:
+		template<typename T>
+		void OnComponentAdded(Entity entity, T& component);
+
 	private:
 		entt::registry m_Registry;
 		uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
