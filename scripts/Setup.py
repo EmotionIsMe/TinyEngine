be3fece905789e4be623051ded1213f440ca3249
EmotionIsMe <616356667@qq.com>
2023-05-04 00:57:57 +0800

    change directory of premake

--- scripts/Setup.py
+++ scripts/Setup.py
@@ -17,4 +17,4 @@ if (not Vulkan.CheckVulkanSDKDebugLibs()):
     print("Vulkan SDK debug libs not found.")
 
 print("Running premake...")
-subprocess.call(["vendor/bin/premake/premake5.exe", "vs2017"])
+subprocess.call(["vendor\premake\bin\premake5.exe", "vs2017"])
