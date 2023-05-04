4a299777e90be78d4624b41550ad4ca90cfd911a
EmotionIsMe <616356667@qq.com>
2023-05-05 00:24:33 +0800

    Added play/stop button to editor

--- Tiny-Editor/src/EditorLayer.h
+++ Tiny-Editor/src/EditorLayer.h
@@ -29,6 +29,12 @@ namespace TinyEngine {
 		void OpenScene();
 		void OpenScene(const std::filesystem::path& path);
 		void SaveSceneAs();
+
+		void OnScenePlay();
+		void OnSceneStop();
+
+		// UI Panels
+		void UI_Toolbar();
 	private:
 		OrthographicCameraController m_CameraController;
 
@@ -58,8 +64,17 @@ namespace TinyEngine {
 
 		int m_GizmoType = -1;
 
+		enum class SceneState
+		{
+			Edit = 0, Play = 1
+		};
+		SceneState m_SceneState = SceneState::Edit;
+
 		// Panels
 		SceneHierarchyPanel m_SceneHierarchyPanel;
 		ContentBrowserPanel m_ContentBrowserPanel;
+
+		// Editor resources
+		Ref<Texture2D> m_IconPlay, m_IconStop;
 	};
 }
