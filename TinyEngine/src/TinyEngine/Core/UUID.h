d9d9708871b12c2ddeebfad311b58a1fa97482d9
EmotionIsMe <616356667@qq.com>
2023-11-11 16:00:01 +0800

    Simplify UUID hash specialization

--- TinyEngine/src/TinyEngine/Core/UUID.h
+++ TinyEngine/src/TinyEngine/Core/UUID.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <xhash>
 
 namespace TinyEngine {
 
@@ -20,12 +19,14 @@ namespace TinyEngine {
 
 namespace std {
 
+	template <typename T> struct hash;
+
 	template<>
 	struct hash<TinyEngine::UUID>
 	{
 		std::size_t operator()(const TinyEngine::UUID& uuid) const
 		{
-			return hash<uint64_t>()((uint64_t)uuid);
+			return (uint64_t)uuid;
 		}
 	};
 
